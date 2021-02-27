8892a147eb7a77e7a79c04c7e06d9e35a7c5efec
Matin Habibi <itsmatinhimself@gmail.com>
2021-02-27 17:22:51 +0330

    Fiexed re-login coockie problem

--- ecent/mixins/account.py
+++ ecent/mixins/account.py
@@ -1,12 +1,13 @@
-
 from typing import List
-from ecent.types import ShortCourse
+
 from ecent.extractor import extract_short_course
 from ecent.mixins.private import PrivateRequest
+from ecent.types import ShortCourse
+
 
 class AccountMixin(PrivateRequest):
-    
+
     def my_courses(self) -> List[ShortCourse]:
+        # TODO: handle access denied response
         home = self.private_request("").text
         return extract_short_course(home)
-  
--- ecent/mixins/auth.py
+++ ecent/mixins/auth.py
@@ -9,21 +9,42 @@ class Auth(PrivateRequest):
         self.cookie = None
         self._auth = self
 
-    def login(self, username: str, password: str) -> bool:
+    def login(self, username: str, password: str, retries: int = 0) -> bool:
+        # prevent infinite recursion
+        if retries > 3:
+            return False
+
+        if self.authorized:
+            self.logout()
+
         self.username = username
         self.password = password
-        logintoken = re.findall(r'name="logintoken" value="(.*?)"',
-                                self.private_request('login/index.php',
-                                                     need_login=False).text)[0]
 
+        login_response = self.private_request('login/index.php', need_login=False)
+        token_group = re.findall(r'name="logintoken" value="(.*?)"', login_response.text)
+
+        if len(token_group) == 0:
+            return False
+        login_token = token_group[0]
+
+        data = f'anchor=&logintoken={login_token}&username={username}&password={password}'
         response = self.private_request('login/index.php',
-                                        data=f'anchor=&logintoken={logintoken}&username={username}&password={password}',
+                                        data=data,
                                         need_login=False)
 
+        self.cookie = {
+            'Cookie': 'MoodleSession={};'.format(
+                response.history[0].cookies.get('MoodleSession')
+            )
+        }
+
+        self.private.headers.update(self.cookie)
         if 'actionmenuaction' in response.text:
             ses_key = re.findall(r'logout.php\?sesskey=(.*?)"', response.text)
             self.session_key = ses_key[0] if len(ses_key) > 0 else None
             self.authorized = True
+        else:
+            self.login(username, password, retries + 1)
 
         return self.authorized
 
