1a8d6deaf6729b09e1993aed99aec570a5e91d12
Matin Habibi <itsmatinhimself@gmail.com>
2021-02-27 15:17:21 +0330

    Added logout and fixed re-Login

--- ecent/mixins/auth.py
+++ ecent/mixins/auth.py
@@ -1,5 +1,7 @@
-from ecent.mixins.private import PrivateRequest
 import re
+
+from ecent.mixins.private import PrivateRequest
+
 
 class Auth(PrivateRequest):
     def __init__(self) -> None:
@@ -11,20 +13,27 @@ class Auth(PrivateRequest):
         self.username = username
         self.password = password
         logintoken = re.findall(r'name="logintoken" value="(.*?)"',
-                        self.private_request('login/index.php',
-                        need_login=False).text)[0]
+                                self.private_request('login/index.php',
+                                                     need_login=False).text)[0]
 
         response = self.private_request('login/index.php',
-                data=f'anchor=&logintoken={logintoken}&username={username}&password={password}',
-                need_login=False)
+                                        data=f'anchor=&logintoken={logintoken}&username={username}&password={password}',
+                                        need_login=False)
 
         if 'actionmenuaction' in response.text:
-            self.cookie = {
-                            'Cookie': 'MoodleSession={};'.format(
-                                response.history[0].cookies.get('MoodleSession')
-                                )
-                          }
-            self.private.headers.update(self.cookie)
+            ses_key = re.findall(r'logout.php\?sesskey=(.*?)"', response.text)
+            self.session_key = ses_key[0] if len(ses_key) > 0 else None
             self.authorized = True
 
         return self.authorized
+
+    def logout(self) -> None:
+        if self.authorized:
+            self.authorized = False
+            response = self.private_request('login/logout.php', params=f'sesskey={self.session_key}', need_login=False)
+            self.cookie = {
+                'Cookie': 'MoodleSession={};'.format(
+                    response.history[0].cookies.get('MoodleSession')
+                )
+            }
+            self.private.headers.update(self.cookie)
